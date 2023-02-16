4ff10ebd1ec58ec047285b0a05ef74b1b05cf507
JorgeDome <115118050+JorgeDome@users.noreply.github.com>
2023-02-15 22:53:50 -0300

    PERRO-04 Añadir funcionalidad de conservar los datos.

---
+++ jsonManager.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const { get } = require("http");
+
+function readFile(){
+    let archivoJson = fs.readFileSync('src/problema.json');
+    let archivo = JSON.parse(archivoJson);
+    return archivo ? archivo : [];
+    
+
+}
+
+function writeFile(perros){
+    let perrosJson = JSON.stringify(perros);
+    fs.writeFileSync('src/problema.json', perrosJson);
+}
+
+function savePerro(perro){
+    let perros = readFile();
+    perros.push(perro);
+    writeFile(perros);
+
+}
+exports.savePerro = savePerro;
+
+function readPerros(){
+    return readFile();
+}
+exports.readPerros = readPerros;
+
+function deletePerro(perroId){
+    let perros = readFile();
+    perros = perros.filter(perro => perro.id != perroId);
+    writeFile(perros);
+
+}
+exports.deletePerro = deletePerro;
+
+
+function editPerro(perro){
+   let perros = readFile();
+   perros = perros.filter(x => x.id != perro.id);
+   perros.push(perro);
+   writeFile(perros);
+   
+}
+exports.editPerro = editPerro;
+
+function getPerro(perroId){
+    let perros = readFile();
+    let perro = perros.find(x => x.id == perroId);
+    
+    return perro
+}
+
+exports.getPerro = getPerro;
+
+
+
+
+
+
+
+
