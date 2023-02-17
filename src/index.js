f394e5063bfad715c0b7d53193b06a584b0cd59e
JorgeDome <115118050+JorgeDome@users.noreply.github.com>
2023-02-17 16:03:32 -0300

    PERR0-05 Migrar los Point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 //settings
 app.set('port', process.env.PORT || 3000);
@@ -12,10 +13,13 @@ app.set('json spaces', 2);
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5500'
+}));
 
 //routes
 app.use(require('./routes/index'));
-app.use('/api/perros',require('./routes/perros'));
+app.use('/api',require('./routes/perros'));
 
 //Empezando el servidor
 app.listen(app.get('port'), ()=> {
--- src/routes/perros.js
+++ src/routes/perros.js
@@ -1,73 +1,42 @@
-const {
-    Router
-} = require('express');
+const { Router } = require('express');
 const router = Router();
-const _ = require('underscore')
+const _ = require('underscore');
+const {
+    v4: uuidv4
+} = require('uuid');
+const jsonManager = require('C:/Users/jorge/OneDrive/Documentos/Trabajo/Perros-Project/Perros-API/src/jsonManager.js');
 
-const perros = require('../datos.json')
-router.get('/', (req, res) => {
-    res.json(perros)
-})
-
-router.post('/', (req, res) => {
-    const {
-        id,
-        nombre,
-        edad,
-        raza,
-        image,
-        descripcion
-    } = req.body;
-    if (id && nombre && edad && raza && image && descripcion) {
-        const id = perros.length + 1
-        const newDog = {
-            ...req.body,
-            id
-        };
-        perros.push(newDog);
-        res.json(perros);
-    } else {
-        res.status(500).json({
-            error: 'Hubo un error'
-        });
-    }
-
+router.get('/perros', (req, res) => {
+    let perros = jsonManager.readPerros();
+    res.json(perros);
 });
 
-
-router.put('/id:', (req,res)=> {
-    const {id}= req.params;
-    const {
-        nombre,
-        edad,
-        raza,
-        image,
-        descripcion
-    } = req.body;
-    if (id && nombre && edad && raza && image && descripcion){
-        _.each(perros, (perro,i)=>{
-            if(perro.id == id){
-                perro.nombre = nombre;
-                perro.edad = edad;
-                perro.raza = raza;
-                perro.image = image;
-                perro.descripcion = descripcion;
-            }
-        });
-        res.json(perros);
-    } else{
-        res.status(500).json({error:'Hubo un error'})
+router.post('/perros', (req, res) => {
+    let perro = req.body;
+    if (!perro.nombre || !perro.edad || !perro.raza || !perro.descripcion || !perro.image) {
+        res.status(400).send("Ingresa todos los datos");
+        return;
     }
+    perro.id = uuidv4();
+    jsonManager.savePerro(perro);
+    res.status(201).send('Perro creado correctamente')
 });
 
-router.delete('/:id', (req, res) => {
+router.put('/perros/:id', (req,res)=> {
     const { id } = req.params;
-    _.each(perros, (perro, i) => {
-        if (perro.id == id) {
-            perros.splice(i, 1);
-        }
-    });
-    res.send(perros);
+    const { nombre, edad, raza, image, descripcion } = req.body;
+    if (id && nombre && edad && raza && image && descripcion){
+        jsonManager.updatePerro(id, nombre, edad, raza, image, descripcion);
+        res.json({ message: 'Perro actualizado correctamente' });
+    } else {
+        res.status(500).json({ error: 'Hubo un error' });
+    }
+});
+
+router.delete('/perros/:id', (req, res) => {
+    const { id } = req.params;
+    jsonManager.deletePerro(id);
+    res.json({ message: 'Perro eliminado correctamente' });
 });
 
 module.exports = router;
